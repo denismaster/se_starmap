3edc6824a5c9b62e2d8b7ff11df4af157db7a005
denismaster <denismaster@outlook.com>
2017-06-15 23:06:43 +0300

    Add Api route to server to response stars for request

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,13 @@ import * as _express from "express";
 import * as _bodyParser from "body-parser";
 import * as _morgan from "morgan";
 import { Request, Response } from "express"
+import { stars } from './stars-repository';
 export class Server {
     public readonly app: _express.Application;
     public readonly port: number;
     public readonly router: _express.Router;
 
-    private pathToFiles = __dirname+"/../../client/";
+    private pathToFiles = __dirname + "/../../client/";
     constructor(port?: number) {
         this.port = port || process.env.PORT || 1234;
         this.app = _express();
@@ -15,16 +16,20 @@ export class Server {
     }
 
     public useDefaultConfig(): Server {
-        this.app.use(_bodyParser.json());   
+        this.app.use(_bodyParser.json());
         this.app.use(_morgan("dev"));
-        
+
         this.app.use(_express.static(this.pathToFiles))
 
-        this.app.use("/",(request:Request,response:Response)=>{
-            response.sendFile("index.html", { root: this.pathToFiles });
-        })
+        this.app.use("/api/stars", (request: Request, response: Response) => {
+            response.end(JSON.stringify(stars));
+        });
 
-        this.app.use((error:any,request:Request,response:Response,next:any)=>{
+        this.app.use("/", (request: Request, response: Response) => {
+            response.sendFile("index.html", { root: this.pathToFiles });
+        });
+
+        this.app.use((error: any, request: Request, response: Response, next: any) => {
             response.status(500);
             console.error(error);
             response.end();

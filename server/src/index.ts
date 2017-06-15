ed9097c7e2c3718ff8977079fddfba7a144b7d1c
Denis Obolensky <denismaster@outlook.com>
2017-06-15 15:14:34 +0300

    Edit title

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import { Server } from './server';
 class Startup {
     public static Main(): void {
-        console.log("DiplomContentSystem LaTeX services");
+        console.log("Mass Space Star map server");
         const server = new Server();
         server.useDefaultConfig().run();
     }

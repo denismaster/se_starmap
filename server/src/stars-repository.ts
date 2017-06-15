6fbd0219c86da1826e2dfac33125b78ab343dca2
denismaster <denismaster@outlook.com>
2017-06-15 23:06:06 +0300

    Add stars as array to server. We can change them to file or db later

---
+++ server/src/stars-repository.ts
@@ -0,0 +1,52 @@
+export const stars = [{
+    name: "Ондерон",
+    x: 580,
+    y: 650,
+    size: 8
+}, {
+    name: "Антарес",
+    x: 675,
+    y: 467,
+    size: 5
+}, {
+    name: "Обан",
+    x: 600,
+    y: 550,
+    size: 15,
+    isDestroyed: true
+}, {
+    name: "Абрегадо-Рэй",
+    x: 330,
+    y: 280,
+    size: 3,
+    owner: "Federation"
+},
+{
+    name: "Коат",
+    x: 480,
+    y: 300,
+    size: 4,
+    owner: "Federation"
+}, {
+    name: "Нор Гейд",
+    x: 400,
+    y: 150,
+    size: 5,
+    owner: "Federation",
+}, {
+    name: "Истергус",
+    x: 429,
+    y: 190,
+    size: 3,
+    owner: "Federation"
+},{
+    name: "LV-223",
+    x: 1890,
+    y: 990,
+    size: 3,
+}, {
+    name: "Шир",
+    x: 1780,
+    y: 120,
+    size: 8
+}];

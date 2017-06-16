c11e02bb6299a7e899a70f1ff5a2cf6c576f6f5e
Denis Obolensky <denis_obolensky@xrm.ru>
2017-06-16 11:55:20 +0300

    Add new stars(first batch)

--- server/src/stars-repository.ts
+++ server/src/stars-repository.ts
@@ -15,31 +15,236 @@ export const stars = [{
     size: 15,
     isDestroyed: true
 }, {
-    name: "Абрегадо-Рэй",
-    x: 330,
-    y: 280,
+    "x": 864,
+    "y": 472,
     size: 3,
-    owner: "Federation"
+    "name": "Эллис",
+    "owner": "Federation"
 },
 {
-    name: "Коат",
-    x: 480,
-    y: 300,
-    size: 4,
-    owner: "Federation"
-}, {
-    name: "Нор Гейд",
-    x: 400,
-    y: 150,
-    size: 5,
-    owner: "Federation",
-}, {
-    name: "Истергус",
-    x: 429,
-    y: 190,
+    "x": 931,
+    "y": 569,
     size: 3,
-    owner: "Federation"
-},{
+    "name": "Абрегадо-Рей",
+    "owner": "Federation"
+},
+{
+    "x": 990,
+    "y": 463,
+    size: 3,
+    "name": "Скирнис",
+    "owner": "Federation"
+},
+{
+    "x": 1184,
+    "y": 618,
+    size: 3,
+    "name": "Саврус",
+    "owner": "Federation"
+},
+{
+    "x": 1013,
+    "y": 334,
+    size: 7,
+    "name": "Нор-Гейд",
+    "owner": "Federation"
+},
+{
+    "x": 1122,
+    "y": 434,
+    size: 3,
+    "name": "Эллис II",
+    "owner": "Federation"
+},
+{
+    "x": 1233,
+    "y": 473,
+    size: 3,
+    "name": "Рамаленр",
+    "owner": "Federation"
+},
+{
+    "x": 1320,
+    "y": 544,
+    size: 3,
+    "name": "Галлен",
+    "owner": "Federation"
+},
+{
+    "x": 1326,
+    "y": 382,
+    size: 3,
+    "name": "Ярин",
+    "owner": "Federation"
+},
+{
+    "x": 1419,
+    "y": 428,
+    size: 3,
+    "name": "Лидирум",
+    "owner": "Federation"
+},
+{
+    "x": 1303,
+    "y": 487,
+    size: 3,
+    "name": "Альфарий",
+    "owner": "Federation"
+},
+{
+    "x": 1427,
+    "y": 537,
+    size: 3,
+    "name": "Криол-Тау",
+    "owner": "Federation"
+},
+{
+    "x": 1488,
+    "y": 471,
+    size: 3,
+    "name": "Эстдайн",
+    "owner": "Federation"
+},
+{
+    "x": 256,
+    size: 3,
+    "y": 1353,
+    "name": "Мельхраг",
+    "owner": "Alliance"
+},
+{
+    "x": 297,
+    "y": 1282,
+    size: 3,
+    "name": "Ауракс",
+    "owner": "Alliance"
+},
+{
+    "x": 401,
+    "y": 1262, size: 3,
+    "name": "Опсида",
+    "owner": "Alliance"
+},
+{
+    "x": 434, size: 3,
+    "y": 1200,
+    "name": "Опсида-2",
+    "owner": "Alliance"
+},
+{
+    "x": 538, size: 3,
+    "y": 1190,
+    "name": "Врата Омегона",
+    "owner": "Alliance"
+},
+{
+    "x": 591, size: 3,
+    "y": 1255,
+    "name": "Омегон",
+    "owner": "Alliance"
+},
+{
+    "x": 655, size: 3,
+    "y": 1310,
+    "name": "Омегон II",
+    "owner": "Alliance"
+},
+{
+    "x": 715, size: 3,
+    "y": 1262,
+    "name": "Омегон III",
+    "owner": "Alliance"
+},
+{
+    "x": 520, size: 3,
+    "y": 1107,
+    "name": "Альфа-Центавра",
+    "owner": "Alliance"
+},
+{
+    "x": 299, size: 3,
+    "y": 1141,
+    "name": "Опсида 3",
+    "owner": "Alliance"
+},
+{
+    "x": 325, size: 3,
+    "y": 1044,
+    "name": "Малендар",
+    "owner": "Alliance"
+},
+{
+    "x": 389, size: 3,
+    "y": 1030,
+    "name": "Сириус",
+    "owner": "Alliance"
+},
+{
+    "x": 556, size: 3,
+    "y": 1036,
+    "name": "Хелган",
+    "owner": "Alliance"
+},
+{
+    "x": 634, size: 3,
+    "y": 1066,
+    "name": "Сирий",
+    "owner": "Alliance"
+},
+{
+    "x": 723, size: 3,
+    "y": 1047,
+    "name": "Новатерра",
+    "owner": "Alliance"
+},
+{
+    "x": 676, size: 3,
+    "y": 992,
+    "name": "Даэдалус",
+    "owner": "Alliance"
+},
+{
+    "x": 443, size: 3,
+    "y": 957,
+    "name": "Солнце",
+    "owner": "Alliance"
+},
+{
+    "x": 498, size: 3,
+    "y": 853,
+    "name": "Альтаир",
+    "owner": "Alliance"
+},
+{
+    "x": 427, size: 3,
+    "y": 811,
+    "name": "Венона",
+    "owner": "Alliance"
+},
+{
+    "x": 323, size: 3,
+    "y": 792,
+    "name": "Кентавр",
+    "owner": "Alliance"
+},
+{
+    "x": 191,
+    "y": 833, size: 3,
+    "name": "Ренин",
+    "owner": "Alliance"
+},
+{
+    "x": 368,
+    "y": 693, size: 3,
+    "name": "Мальрок",
+    "owner": "Alliance"
+},
+{
+    "x": 198, size: 3,
+    "y": 747,
+    "name": "Бисмарк",
+    "owner": "Alliance"
+}, {
     name: "LV-223",
     x: 1890,
     y: 990,
@@ -49,4 +254,23 @@ export const stars = [{
     x: 1780,
     y: 120,
     size: 8
-}];
+},
+{ "x": 1046, size: 3, "y": 726, "name": "Кассиния", "owner": "Empire" },
+{ "x": 1123, "y": 780, size: 3, "name": "Нираган", "owner": "Empire" },
+{ "x": 1002, "y": 794, size: 3, "name": "Варул", "owner": "Empire" },
+{ "x": 1166, "y": 808, size: 3, "name": "Ферриска", "owner": "Empire" },
+{ "x": 1021, "y": 859, size: 3, "name": "Меран", "owner": "Empire" },
+{ "x": 1148, "y": 863, size: 3, "name": "Омекрон", "owner": "Empire" },
+{ "x": 1095, "y": 890, size: 3, "name": "Палте-Хагадан", "owner": "Empire" },
+{ "x": 942, "y": 911, size: 3, "name": "Псалион", "owner": "Empire" },
+{ "x": 1238, "y": 942, size: 3, "name": "Гулиар", "owner": "Empire" },
+{ "x": 926, "y": 969, size: 3, "name": "Псилион", "owner": "Empire" },
+{ "x": 1137, "y": 1022, size: 3, "name": "Хагадан", "owner": "Empire" },
+{ "x": 1236, "y": 1087, size: 3, "name": "Рени", "owner": "Empire" },
+{ "x": 958, "y": 1173, size: 3, "name": "Дальний", "owner": "Empire" },
+{ "x": 1210, "y": 1153, size: 3, "name": "Элизиум", "owner": "Empire" },
+{ "x": 1295, "y": 1178, size: 3, "name": "Лзорон", "owner": "Empire" },
+{ "x": 1170, "y": 1196, size: 3, "name": "Жакрегор", "owner": "Empire" },
+{ "x": 1150, "y": 1268, size: 3, "name": "Сринирус", "owner": "Empire" },
+{ "x": 1214, "y": 1311, size: 3, "name": "Чира", "owner": "Empire" },
+{ "x": 1032, "y": 1373, size: 3, "name": "Шрекон", "owner": "Empire" }];
